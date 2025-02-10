55526a047038a45e560fc589337a6525c85a5230
Kyrky <kyrkyrqq@gmail.com>
2025-02-10 20:37:47 +0200

    intro

--- js/intro.js
+++ js/intro.js
@@ -1,10 +1,16 @@
 $(document).ready(function() {
-    $('.intro').animate({ opacity: 1 }, 500, function() {
+    if ($(window).width() >= 1000) {
+      $('.intro').animate({ opacity: 1 }, 500, function() {
         $('.content').animate({ opacity: 1 }, 2500);
         setTimeout(function() {
-            $('.intro').animate({ opacity: 0 }, 2500, function() {
-                $('body').css('filter', 'none');
-            });
+          $('.intro').animate({ opacity: 0 }, 2500, function() {
+            $('body').css('filter', 'none');
+          });
         }, 500);
-    });
-});
+      });
+    } else {
+      $('.intro').remove();
+      $('.content').css('opacity', 1);
+    }
+  });
+  
